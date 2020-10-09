710b035ea87ff49d4b851bb00a2bbbfce9a30cec
UmeshChandra123 <72588179+UmeshChandra123@users.noreply.github.com>
2020-10-09 18:31:27 +0530

    Create test5.py

---
+++ test5.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number"))
+b = int(input("Enter the first number"))
+c = int(input("Enter the first number"))
+d = int(input("Enter the first number"))
+e = int(input("Enter the first number"))
+f = a+b+c+d+e
+print(f)
